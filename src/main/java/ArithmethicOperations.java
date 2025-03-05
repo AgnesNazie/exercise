0d1e105d80fb60702d9ad00aa19dd39fd0060bff
AgnesNazie <cheagnesmbo@gmail.com>
2025-03-05 16:05:59 +0100

    fixed bug

--- src/main/java/ArithmethicOperations.java
+++ src/main/java/ArithmethicOperations.java
@@ -1,15 +1,15 @@
 
 public class ArithmethicOperations {
     public static void main (String[] args) {
-        int sum = 45 + 11;
+        int addition = 45 + 11;
         int multiplication =12 * 4;
-        int divisionn = 24 / 6;
+        int division = 24 / 6;
         int substraction = 55 - 12;
 
-        System.out.println("45 + 11 " + addition);
-        System.out.println("12 * 4" + multiplication);
-        System.out.println(" 24 / 6" + division);
-        System.out.println("55 - 12" + substraction);
+        System.out.println("45 + 11 =" +  addition);
+        System.out.println("12 * 4 =" +  multiplication);
+        System.out.println("24 / 6 =" +  division);
+        System.out.println("55 - 12 =" +  substraction);
 
     }
 }
