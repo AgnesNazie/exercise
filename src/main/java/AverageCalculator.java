8a5316aeae4fcf8476786bb75723886b42dbc59c
AgnesNazie <cheagnesmbo@gmail.com>
2025-03-05 16:16:43 +0100

    completed a java program that prints the average of 3 numbers

--- src/main/java/AverageCalculator.java
+++ src/main/java/AverageCalculator.java
@@ -1,6 +1,11 @@
 
 public class AverageCalculator {
     public static void main(String[] args) {
-
+      int  number = 23;
+      int number1 = 11;
+      int number2 = 77;
+      int sum = number + number1 + number2;
+      int average =sum / 3;
+        System.out.println("("+ number + "+"  + number1 + "+" + number2 + ")/3 =" + average);
     }
 }
