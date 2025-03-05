16f54016822aefdb87d92fc767cbda1cab93ed17
AgnesNazie <cheagnesmbo@gmail.com>
2025-03-05 16:55:45 +0100

    created an input program that allows username perform arithmethic operations

---
+++ src/main/java/UserArithmethicOperations.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class UserArithmethicOperations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your first number");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Enter your second number");
+        int num2 = scanner.nextInt();
+
+        System.out.println(num1 + "+" + num2 + "=" + (num1 + num2));
+        System.out.println(num1 + "+" + num2 + "=" + (num1 * num2));
+        System.out.println(num1 + "+" + num2 + "=" + (num1 / num2));
+        System.out.println(num1 + "+" + num2 + "=" + (num1- num2));
+        scanner.close();
+
+    }
+}
