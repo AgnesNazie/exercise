46443e67d1ca7217016f2d709555dab38d35490d
AgnesNazie <cheagnesmbo@gmail.com>
2025-03-05 15:04:44 +0100

    check bug

--- src/main/java/com/agnes/Main.java
+++ src/main/java/com/agnes/Main.java
@@ -3,6 +3,6 @@ package com.agnes;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("Agnes!");
+        System.out.println("Nazie!");
     }
 }
