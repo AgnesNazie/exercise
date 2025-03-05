d75484929ad314aea49d361ec6acf73c7fd99dca
AgnesNazie <cheagnesmbo@gmail.com>
2025-03-05 15:48:05 +0100

    completed exercise 3

---
+++ src/main/java/ArithmethicOperations.java
@@ -0,0 +1,17 @@
+
+public class ArithmethicOperations {
+    public static void main (String[] args) {
+        int sumAndSubstract = 85 + 10 -60;
+        int division = 85 / 10;
+        int substractAndMultiplication = 85 - 10 * 5;
+        int multiplication = 85 * 10;
+        int multiplicationAndQouteint = 85 * 10 % 5;
+
+        System.out.println("85 + 10 -60 " + sumAndSubstract);
+        System.out.println("85 / 10" + division);
+        System.out.println("85 - 10 * 5" + substractAndMultiplication);
+        System.out.println(" 85 * 10" + multiplication);
+        System.out.println("85 * 10 % 5 " + multiplicationAndQouteint);
+
+    }
+}
