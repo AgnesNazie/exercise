f60b6fbc2c42129cabe527ddf0dd4c8394b67b0f
AgnesNazie <cheagnesmbo@gmail.com>
2025-03-05 16:40:43 +0100

    created an input program that ask for username

--- src/main/java/com/agnes/Main.java
+++ src/main/java/com/agnes/Main.java
@@ -1,8 +1,16 @@
 package com.agnes;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("Nazie!");
+        System.out.println("Agnes!");
+      Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your name:");
+        String userName = scanner.nextLine();
+        System.out.println("Hello " + userName);
+        scanner.close();
+
     }
 }
